42dac004c2aeda3423a4bed7fa0d616d68d5c2b3
JanFidor <jan.p.fidor@gmail.com>
2022-11-26 00:48:34 +0100

    fix minor mistake

--- database/schema/user.py
+++ database/schema/user.py
@@ -3,18 +3,17 @@ import uuid
 from badge import Badge
 import sqlite3 as sql
 
-table_name = "submissions"
 
 class user(BaseModel):
-    def __int__(
-        self,
-        nick: str,
-        email: str,
-        password: str,
-        points: int = 0,
-        address: str = None,
-        phone_number: str = None,
-        id: uuid.UUID = None,
+    def __init__(
+            self,
+            nick: str,
+            email: str,
+            password: str,
+            points: int = 0,
+            address: str = None,
+            phone_number: str = None,
+            id: uuid.UUID = None,
     ):
         self.id = id or uuid.uuid4()
         self.nick = nick
@@ -26,23 +25,22 @@ class user(BaseModel):
 
     def insert(self):
         query = "INSERT INTO " \
-                f"{table_name} (ID, NICK, EMAIL, PASSWORD, POINTS, ADDRESS, PHONE_NUMBER)" \
+                "users (ID, NICK, EMAIL, PASSWORD, POINTS, ADDRESS, PHONE_NUMBER)" \
                 "VALUES(?, ?, ?, ?, ?, ?, ?);"
 
         values = (self.id, self.nick, self.email, self.password, self.points, self.address, self.phone_number)
 
-        sql.connect(table_name).execute(query, values)
+        sql.connect("users").execute(query, values)
 
     def delete(self):
-        query = f"DELETE FROM {table_name} WHERE ID=?;"
+        query = "DELETE FROM users WHERE ID=?;"
 
-        sql.connect(table_name).execute(query, (self.id,))
+        sql.connect("users").execute(query, (self.id,))
 
     def update(self):
-        query = f"UPDATE table SET " \
-                f"NICK=?, EMAIL=?, PASSWORD=?, POINTS=?, ADDRESS=?, PHONE_NUMBER=?" \
-                f"WHERE ID=?"
-
-        sql.connect(table_name).execute(query, (self.id,))
-
-
+        query = "UPDATE users SET " \
+                "NICK=?, EMAIL=?, PASSWORD=?, POINTS=?, ADDRESS=?, PHONE_NUMBER=?" \
+                "WHERE ID=?"
+        values = (self.nick, self.email, self.password, self.points, self.address, self.phone_number)
+        params = values + (self.id,)
+        sql.connect("users").execute(query, params)
