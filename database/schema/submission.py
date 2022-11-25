9c41500a4f7e18ef9ae115bb023d101880d3dff4
JanFidor <jan.p.fidor@gmail.com>
2022-11-25 22:03:53 +0100

    add submission insert & delete

--- database/schema/submission.py
+++ database/schema/submission.py
@@ -1,5 +1,32 @@
 from pydantic import BaseModel
+import uuid
+import datetime
+import sqlite3 as sql
+
+table_name = "submissions"
 
 
 class Submissions(BaseModel):
-    pass
+    def __int__(
+            self,
+            post_time: datetime.datetime,
+            content: str,
+            user_id: uuid.UUID = None,
+    ):
+        self.user_id: uuid.UUID = user_id or uuid.uuid4()
+        self.post_time = post_time
+        self.content = content
+
+    def insert(self):
+        query = "INSERT INTO " \
+                f"{table_name} (USER_ID, POST_TIME, CONTENT)" \
+                "VALUES(?, ?, ?);"
+
+        sql.connect(table_name).execute(query, (self.user_id, self.post_time, self.content))
+
+    def delete(self):
+        query = f"DELETE FROM {table_name} WHERE USER_ID=?;"
+
+        sql.connect(table_name).execute(query, (self.user_id,))
+
+
