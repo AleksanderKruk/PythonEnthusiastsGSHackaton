50935c33ef30852c6f0d8b4fffd1b8d9bdd96e12
Aleksander Kruk <aleksanderjakubkruk@gmail.com>
2022-11-26 03:42:16 +0100

    refactored gadget

--- database/schema/gadget.py
+++ database/schema/gadget.py
@@ -1,33 +1,36 @@
 #from pydantic import BaseModel
 import uuid
 import sqlite3 as sql
+from pydantic import BaseModel
 
-class Gadgets:
+class Gadget(BaseModel):
     id: uuid.UUID
     name: str
     point_cost: int
     image_url: str
 
-    def __init__(self, uuid: uuid, name: str, point_cost: int, image_url: str):
-        self.id = uuid
-        self.name = name
-        self.point_cost = point_cost
-        self.image_url = image_url
+    @staticmethod
+    def from_tuple(tup):
+        return Gadget(
+            id=tup[0],
+            name=tup[1],
+            point_cost=tup[2],
+            image_url=tup[3]
+        )
 
-    def insert(self):
-        sql.connect("database\gs.db").execute(
+    def insert(self, con):
+        con.execute(
             "INSERT INTO ? (id, name, point_cost, image_url) VALUES(?, ?, ?, ?);",
             (self.__class__.__name__, self.id, self.name, self.point_cost, self.image_url)
         )
 
-
-    def delete(self):
-        sql.connect("database\gs.db").execute(
+    def delete(self, con):
+        con.execute(
             "DELETE FROM ? WHERE id = ?;", (self.__class__.__name__, self.id)
         )
 
-    def update(self):
-        sql.connect("database\gs.db").execute(
+    def update(self, con):
+        con.execute(
             "UPDATE ? SET name = ?, point_cost = ?, image_url = ?;",
             (self.__class__.__name__, self.name, self.point_cost, self.image_url)
         )
