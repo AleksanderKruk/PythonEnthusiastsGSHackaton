9e297ff56d309e5ba70d0d3f933ee60a1f2bb20e
tslowinski <01159096@pw.edu.pl>
2022-11-26 06:31:05 +0100

    challenge

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from uvicorn import run
 import sqlite3 as sql
 from database import user_utility
@@ -6,6 +8,7 @@ from fastapi.requests import Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from database.schema.submission import Submission
 
 app = FastAPI()
 conn = sql.connect('database/gs.db')
@@ -56,13 +59,48 @@ async def get_home(request: Request):
     res = user_utility.validate_token(token, conn)
     print(res)
     q = 'SELECT * FROM challenges'
-    challenges = conn.execute(q).fetchall() or [(1, 'lol'), (2, 'lmao')]
+    challenges = conn.execute(q).fetchall()
+    print(len(challenges))
 
     if res is None:
         return RedirectResponse('/login', status_code=303)
 
     return templates.TemplateResponse("home.html", {"request": request, "id": id, "content": challenges})
 
+@app.get('/challenge/{id}')
+async def get_challenge(id: str, request: Request):
+    q = 'SELECT * FROM challenges WHERE id=?'
+    challenge = conn.execute(q, (id,)).fetchone()
+
+    if challenge is None:
+        return RedirectResponse('/home', status_code=303)
+    print(challenge)
+    return templates.TemplateResponse("challenge.html", {"request": request, "id": id, "challenge": challenge})
+
+
+@app.post('/challenge/{id}')
+async def get_challenge(id: str, request: Request, submission=Form()):
+    q = 'SELECT * FROM challenges WHERE id=?'
+    challenge = conn.execute(q, (id,)).fetchone()
+
+    if challenge is None:
+        return RedirectResponse('/home', status_code=303)
+
+    # update punktów na danym użytkowniku w DB
+    ####
+    token = request.cookies.get('token')
+    res = list(user_utility.validate_token(token, conn))
+    res[4] += 1
+    q = 'UPDATE users SET points=? WHERE id=?'
+    conn.execute(q, (res[4], res[0]))
+
+    # wstawianie submisssion
+    s = Submission(user_id=res[0], challenge_id=id, posting_iso=str(datetime.datetime.now()), text=submission)
+    s.insert(conn)
+    conn.commit()
+
+    # wraca do home'a (z ewentualnym dodatkowym komunikatem
+    return RedirectResponse('/home', status_code=303)
 
 if __name__ == '__main__':
     try:
